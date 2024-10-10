747d3bf5614792d073d5daadb852bb57cea5bb72
Umair Habib <128183479+UMAIRH1@users.noreply.github.com>
2024-10-10 23:01:30 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        bgcolor: "#1f2020",
+        customGray: "#2d2d2d", // Add the custom color here
+      },
+    },
+  },
+  plugins: [],
+};
